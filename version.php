db9ff8b91b9fc3960280f30c293f2107c2ee7a69
Mattan Dor <mattan.dor1@gmail.com>
2024-05-14 17:07:02 +0300

    Added: increase version plugins.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024051500;
+$plugin->version = 2024051600;
 $plugin->requires = 2022041900.00;
 $plugin->release = 'v1.0.4';
 $plugin->component = 'mod_stream';
