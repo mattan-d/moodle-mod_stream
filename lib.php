ad5a97576b213d21abbc7d7b702b7949104fb86a
Mattan Dor <mattan.dor1@gmail.com>
2024-06-07 02:40:10 +0300

    Fixed: call to Stream endpoint with CURL_HTTP_VERSION_1_1.

--- lib.php
+++ lib.php
@@ -55,7 +55,7 @@ function stream_supports($feature) {
  * of the new instance.
  *
  * @param stdClass $stream Submitted data from the form in mod_form.php
- * @param mod_stream_mod_form $mform The form instance itself (if needed)
+ * @param $mform The form instance itself (if needed)
  * @return int The id of the newly inserted stream record
  */
 function stream_add_instance(stdClass $stream, $mform = null) {
