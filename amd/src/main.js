e3a03e45455d402ebad089f59cf17f155cb476a4
Mattan Dor <mattan.dor1@gmail.com>
2024-11-01 13:19:06 +0200

    Fixed: JavaScript ESLint Errors Fixes.

--- amd/src/main.js
+++ amd/src/main.js
@@ -110,18 +110,20 @@ define(['jquery', 'core/ajax', 'core/notification', 'core/str', 'core/url'], fun
           self.elements.html('');
 
           $.each(response.videos, function(key, video) {
-
             str.get_strings([{'key': 'views', component: 'mod_stream'}, {'key': 'before', component: 'mod_stream'}]).
                 then(function(string) {
-                  var html = '<div class="col list-item-grid" data-itemid="' + video.id + '" ' + 'id="video_identifier_' +
-                      video.id + '">' + '<span class="item" ><div class="thumbnail">' + '<img src="' + video.thumbnail +
+                  var html = '<div class="col list-item-grid" data-itemid="' + video.id + '" id="video_identifier_' +
+                      video.id + '">' + '<span class="item"><div class="thumbnail">' + '<img src="' + video.thumbnail +
                       '" class="img-fluid img-rounded">' + '<span class="datecreated">' + video.datecreated +
                       '</span><span class="duration">' + video.duration + '</span></div><span class="title">' + video.title +
                       '</span><span class="details">' + video.views + ' ' + string[0] + ' <span class="bubble">●</span>' + ' ' +
                       string[1] + ' ' + video.elapsed + '</span></span></div>';
                   self.elements.append(html);
-                });
+                  return null;  // Ensure this `then` returns something
+                }).
+                catch(function(error) {});
           });
+
         } else {
           return str.get_string('noresults', 'mod_stream').then(function(noresults) {
             return self.elements.html('<div class="alert alert-info">' + noresults + '</div>');
