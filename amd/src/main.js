a61ed34c37188353c54f7a8cc4989e70931d4659
Mattan Dor <mattan.dor1@gmail.com>
2024-11-01 08:58:59 +0200

    Fixed: File is stale and needs to be rebuilt: amd/build/init.min.js.map

--- amd/src/main.js
+++ amd/src/main.js
@@ -76,7 +76,6 @@ define(['jquery', 'core/ajax', 'core/notification', 'core/str', 'core/url'], fun
                     'id="video_identifier_' + event.data.streamid + '"><span class="item selected"><div class="thumbnail">' +
                     '<img src="' + event.data.thumbnail + '" class="img-fluid img-rounded">' +
                     '</div><span class="title">' + event.data.topic + '</span></span></div>';
-
                 self.elements.append(html);
             }
         }, load: function() {
@@ -120,12 +119,15 @@ define(['jquery', 'core/ajax', 'core/notification', 'core/str', 'core/url'], fun
 
                         str.get_strings([{'key': 'views', component: 'mod_stream'}, {'key': 'before', component: 'mod_stream'}])
                             .then(function(string) {
-
-                                var html = '<div class="col list-item-grid" data-itemid="' + event.data.streamid + '" ' +
-                                    'id="video_identifier_' + event.data.streamid + '"><span class="item selected">' +
-                                    '<div class="thumbnail"><img src="' + event.data.thumbnail + '" class="img-fluid img-rounded">' +
-                                    '</div><span class="title">' + event.data.topic + '</span></span></div>';
-
+                                var html = '<div class="col list-item-grid" data-itemid="' + video.id + '" ' +
+                                    'id="video_identifier_' + video.id + '">' +
+                                    '<span class="item" ><div class="thumbnail">' +
+                                    '<img src="' + video.thumbnail + '" class="img-fluid img-rounded">' +
+                                    '<span class="datecreated">' + video.datecreated + '</span><span class="duration">' +
+                                    video.duration + '</span></div><span class="title">' + video.title +
+                                    '</span><span class="details">' + video.views + ' ' + string[0] +
+                                    ' <span class="bubble">●</span>' +
+                                    ' ' + string[1] + ' ' + video.elapsed + '</span></span></div>';
                                 self.elements.append(html);
                             });
                     });
