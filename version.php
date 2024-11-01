4abf508d7260d573851c6db2ff8557f45d5daa5d
Mattan Dor <mattan.dor1@gmail.com>
2024-11-01 13:38:04 +0200

    Incremented plugin version for latest updates.

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024103100;
+$plugin->version = 2024110100;
 $plugin->requires = 2017111300;
-$plugin->release = 'v1.2';
+$plugin->release = 'v1.3';
 $plugin->component = 'mod_stream';
 $plugin->maturity = MATURITY_STABLE;
